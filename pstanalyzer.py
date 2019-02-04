c8f944e973fd4bac72ef82f4829c89b2420b96e4
contiser <contiser@students.zhaw.ch>
2019-02-04 22:16:58 +0100

    using parseaddr function to get emails, excluding TL folders from received items

--- pstanalyzer.py
+++ pstanalyzer.py
@@ -7,6 +7,7 @@ import sys
 import pypff
 import operator
 import re
+from email.utils import parseaddr
 
 
 def openPST():
@@ -48,6 +49,7 @@ def parseSentItems(folders):
         # print('Parsing for Sent Items:' + folders[folder].get_name())
         if (folders[folder].get_name() == "Posta inviata") or (folders[folder].get_name() == "Posta Inviata") \
                 or (folders[folder].get_name() == "Sent items") or (folders[folder].get_name() == "Sent Items") \
+                or (folders[folder].get_name() == "Sent Messages") \
                 or (folders[folder].get_name() == "Gesendete Elemente") or (
                 folders[folder].get_name() == "Messages envoyés"):
             return folders[folder]
@@ -56,12 +58,25 @@ def parseSentItems(folders):
 def parseReceivedItems(folders):
     # If we weren't lucky, well... let's try to do the inverse with the inbox and the 'recipient' field
     foldersToAnalyze = []
+    root_folder = pstfile.get_root_folder()
+    isroot = bool
     for folder in range(0, len(folders)):
         # print('Parsing for inbox:' + folders[folder].get_name())
-        if not (folders[folder].get_name() == "Posta inviata") and not (folders[folder].get_name() == "Posta Inviata") \
-                and not (folders[folder].get_name() == "Sent items") and not (folders[folder].get_name() == "Sent Items") \
-                and not (folders[folder].get_name() == "Gesendete Elemente") and not (
-                folders[folder].get_name() == "Messages envoyés"):
+        for root_subfolder in range(0, root_folder.get_number_of_sub_folders()):
+            if folders[folder].get_name() == root_folder.get_sub_folder(root_subfolder).get_name():
+                isroot = 1
+                break
+            else:
+                isroot = 0
+        if not isroot \
+                and not (folders[folder].get_name() == "Posta inviata") \
+                and not (folders[folder].get_name() == "Posta Inviata") \
+                and not (folders[folder].get_name() == "Sent items") \
+                and not (folders[folder].get_name() == "Sent Items") \
+                and not (folders[folder].get_name() == "Sent Messages") \
+                and not (folders[folder].get_name() == "Drafts") \
+                and not (folders[folder].get_name() == "Gesendete Elemente") \
+                and not (folders[folder].get_name() == "Messages envoyés"):
             foldersToAnalyze.append(folders[folder])
     return foldersToAnalyze
 
@@ -69,7 +84,7 @@ def parseReceivedItems(folders):
 def lookForSender(sentItems):
     global processedSentItems
     for message in range(0, sentItems.get_number_of_sub_messages()):
-        if len(sentItems.get_sub_message(message).get_sender_name())>1:
+        if len(sentItems.get_sub_message(message).get_sender_name()) > 1:
             if sentItems.get_sub_message(message).get_sender_name() in senders:
                 senders[sentItems.get_sub_message(message).get_sender_name()] = \
                     senders[sentItems.get_sub_message(message).get_sender_name()] + 1
@@ -96,7 +111,7 @@ def lookForRecipient(receivedItems):
     global processedReceivedItems
     for message in range(0, receivedItems.get_number_of_sub_messages()):
         users = getRecipient(receivedItems.get_sub_message(message))
-        if len(users) >=1:
+        if len(users) >= 1:
             for user in range(0, len(users)):
                 actualUser = users[user]
                 if actualUser in recipients:
@@ -110,9 +125,9 @@ def lookForRecipient(receivedItems):
 
 def getRecipient(message):
     try:
-        to = re.findall("^To: \S*.*\d*@\S+.+\S+", message.transport_headers, re.MULTILINE)
+        to = re.findall(r"^To: \S*.*\d*@\S+.+\S+", message.transport_headers, re.MULTILINE)
         for recipient in range(0, len(to)):
-            to[recipient] = to[recipient].strip("To: ").strip("<").strip(">").strip(" ").strip("\"").strip(" <")
+            to[recipient] = parseaddr(to[recipient])[1]
         return to
     except TypeError:
         return []
